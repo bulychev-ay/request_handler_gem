3a9395d7492921d39ff51be039f4ae58a9a0e69c
bulychev_ay <lupustest@gmail.com>
2018-12-10 17:14:02 +0500

    Add dependencies

--- lib/request_handler_gem.rb
+++ lib/request_handler_gem.rb
@@ -1,5 +1,8 @@
 class RequestHandlerGem
 
+   require 'local_storage_modules/p_store_handler.rb'
+   require 'local_storage_modules/redis_handler.rb'
+   
    @@using_handler = if ENV['STORAGE_TYPE'].eql? 'redis'
       LocalStorageModules::RedisHandler
    else
